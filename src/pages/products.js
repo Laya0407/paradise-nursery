ec10b0563c1ae4444daed10906712d68f0b9d28e
Laya0407 <layanika46@gmail.com>
2024-07-13 21:25:56 +0000

    Add LandingPage component with basic structure and styling link

---
+++ src/pages/products.js
@@ -0,0 +1,12 @@
+// src/data/products.js
+const products = [
+  {
+    id: 1,
+    name: 'Fiddle Leaf Fig',
+    price: 25.99,
+    image: 'path-to-fiddle-leaf-fig.jpg'
+  },
+  // Add more products
+];
+
+export default products;
